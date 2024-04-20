d13b11805bb62347aebdc5ab2d902a16d1d0caf2
Jeff Aknine <j.aknine@matawan-mobility.com>
2024-04-20 17:14:10 +0200

    test with image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:score_card/extension.dart';
 import 'package:score_card/models/hole.dart';
 
@@ -19,7 +20,114 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green.shade100),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const RealCard(),
+    );
+  }
+}
+
+class RealCard extends StatefulWidget {
+  const RealCard({super.key});
+
+  @override
+  State<RealCard> createState() => _RealCardState();
+}
+
+class _RealCardState extends State<RealCard> {
+  final holes = [
+    Hole(number: 1, par: 4, handicap: 5),
+    Hole(number: 2, par: 5, handicap: 3),
+    Hole(number: 3, par: 4, handicap: 11),
+    Hole(number: 4, par: 3, handicap: 15),
+    Hole(number: 5, par: 5, handicap: 17),
+    Hole(number: 6, par: 5, handicap: 1),
+    Hole(number: 7, par: 3, handicap: 13),
+    Hole(number: 8, par: 4, handicap: 7),
+    Hole(number: 9, par: 3, handicap: 9),
+    Hole(number: 10, par: 4, handicap: 16),
+    Hole(number: 11, par: 5, handicap: 6),
+    Hole(number: 12, par: 3, handicap: 4),
+    Hole(number: 13, par: 5, handicap: 12),
+    Hole(number: 14, par: 4, handicap: 2),
+    Hole(number: 15, par: 3, handicap: 8),
+    Hole(number: 16, par: 4, handicap: 14),
+    Hole(number: 17, par: 3, handicap: 10),
+    Hole(number: 18, par: 5, handicap: 18),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    int getScore({int take = 18}) {
+      return holes.take(take).fold<int>(
+          0,
+          (previous, element) =>
+              previous + (element.score != null ? element.score! - element.par : 0));
+    }
+
+    int getBrut({int take = 18}) {
+      return holes.take(take).fold(0,
+          (previousValue, element) => previousValue + (element.score != null ? element.score! : 0));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ma carte'),
+      ),
+      body: InteractiveViewer(
+        minScale: .00001,
+        maxScale: 100000,
+        child: Stack(
+          children: [
+            Image.asset('assets/score_card.png'),
+            Positioned(
+              top: 410,
+              left: -68,
+              child: Transform.scale(
+                scale: .5,
+                child: Form(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: holes
+                        .take(9)
+                        .mapIndexed((h, i) => SizedBox(
+                              width: 74,
+                              child: TextField(
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(fontSize: 32),
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  isDense: true,
+                                  contentPadding:
+                                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 0),
+                                ),
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly,
+                                  TextInputFormatter.withFunction((oldValue, newValue) =>
+                                      newValue.text == '0' ? oldValue : newValue)
+                                ],
+                                onChanged: (value) => setState(() {
+                                  holes[i].score = int.tryParse(value);
+                                }),
+                              ),
+                            ))
+                        .toList(),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+                top: 417,
+                left: 430,
+                child: Transform.scale(
+                  scale: .5,
+                  child: Text(
+                    "${getBrut(take: 9)}",
+                    style: const TextStyle(fontSize: 32),
+                  ),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
