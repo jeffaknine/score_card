83720fe267f813d5de40abbf4076f430279f9e45
Jeff Aknine <j.aknine@matawan-mobility.com>
2025-04-14 21:13:41 +0200

    Refactor main.dart and hole.dart; add HomePage widget with hole details and distance calculations

--- lib/models/hole.dart
+++ lib/models/hole.dart
@@ -1,12 +1,44 @@
+enum TeeColor {
+  black,
+  white,
+  yellow,
+  blue,
+  red,
+}
+
 class Hole {
   final int number;
   final int par;
   final int handicap;
+  final int distanceBlack;
+  final int distanceWhite;
+  final int distanceYellow;
+  final int distanceBlue;
+  final int distanceRed;
   Hole({
     required this.number,
     required this.par,
     required this.handicap,
+    this.distanceBlack = 0,
+    this.distanceWhite = 0,
+    this.distanceYellow = 0,
+    this.distanceBlue = 0,
+    this.distanceRed = 0,
   });
 
   int? score;
+  int distance(TeeColor color) {
+    switch (color) {
+      case TeeColor.black:
+        return distanceBlack;
+      case TeeColor.white:
+        return distanceWhite;
+      case TeeColor.yellow:
+        return distanceYellow;
+      case TeeColor.blue:
+        return distanceBlue;
+      case TeeColor.red:
+        return distanceRed;
+    }
+  }
 }
